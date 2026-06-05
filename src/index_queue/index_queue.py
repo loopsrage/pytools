44584b6ea30cbe0d720adb089ab7a036c758c0e0
jarek <loop123jk@gmail.com>
2026-06-04 20:42:21 -0600

    abstraction discovered

--- src/index_queue/index_queue.py
+++ src/index_queue/index_queue.py
@@ -1,7 +1,9 @@
 import asyncio
+import threading
 import traceback
 
 from meta_models.working_service import WorkerActionService, WorkerServiceConfig
+from queue_controller.helpers import new_controller
 from queue_controller.queueController import QueueController
 from queue_controller.queueData import QueueData
 from thread_safe.index import Index
@@ -17,9 +19,14 @@ class ActionConfig:
 
 class IndexQueue:
     index: Index = None
+    _counter_lock = None
 
     def __init__(self, action: dict[str, ActionConfig]):
         self.index = Index()
+        self._in_flight_tasks = 0
+        self._loop = asyncio.get_running_loop()
+        self._counter_lock = threading.Lock()
+        self._all_tasks_done = asyncio.Event()
         for name, config in action.items():
             self.index.new(name)
             config.config = WorkerServiceConfig(
@@ -30,6 +37,7 @@ class IndexQueue:
             )
             config.service = self.producer(name, config.config)
             self.store_stage_action(name, config)
+            self._counter_lock = threading.Lock()
 
     def producer(self, action_key: str, config):
         async def action():
@@ -58,5 +66,42 @@ class IndexQueue:
         return self.index.load_from_index("STAGE_ACTIONS", name)
 
     def enqueue(self, action_key: str, key, value):
+        with self._counter_lock:
+            self._in_flight_tasks += 1
+            if self._in_flight_tasks == 1:
+                self._all_tasks_done.clear()
+
         self.index.store_in_index(action_key, key, value)
 
+    def task_complete(self):
+        with self._counter_lock:
+            if self._in_flight_tasks > 0:
+                self._in_flight_tasks -= 1
+                if self._in_flight_tasks == 0:
+                    self._loop.call_soon_threadsafe(self._all_tasks_done.set)
+
+    async def wait_for_completions(self):
+        with self._counter_lock:
+            if self._in_flight_tasks == 0:
+                return
+        await self._all_tasks_done.wait()
+
+def new_index_queue(worker_count: int, *actions):
+    _closed_workers_tracker = 0
+    _tracker_lock = threading.Lock()
+    _actions = {}
+
+    for x in actions:
+        def a(target_action):
+            def b(queue_data: QueueData):
+                index = queue_data.attribute("index_queue")
+                try:
+                    target_action(index, queue_data)
+                except Exception as e:
+                    traceback.print_exception(e)
+                finally:
+                    index.task_complete()
+            return b
+        _actions[x.__name__] = ActionConfig(queue=new_controller(action=a(x), worker_count=worker_count))
+
+    return _actions
