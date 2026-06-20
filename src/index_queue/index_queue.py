f9181f6cfa1702fc420bd523d7d66858bc88ed74
jarek <loop123jk@gmail.com>
2026-06-20 17:10:56 -0600

    throttling per action implemented

--- src/index_queue/index_queue.py
+++ src/index_queue/index_queue.py
@@ -25,14 +25,22 @@ class IndexQueue:
 
     def __init__(self, action: dict[str, ActionConfig]):
         self.index = Index()
+        self.max_index_size = 256
+        self._counter_lock = threading.Condition(lock=threading.Lock())
         self._in_flight_tasks = 0
         self._loop = asyncio.get_running_loop()
-        self._counter_lock = threading.Lock()
         self._all_tasks_done = asyncio.Event()
+
+        self._in_flight_tasks: dict[str, int] = {}
+        self._max_sizes: dict[str, int] = {}
+
+
         self._pbar = tqdm(desc="Processing Tasks", unit="task", total=0)
 
         for name, config in action.items():
             self.index.new(name)
+            self._in_flight_tasks[name] = 0
+            self._max_sizes[name] = self.max_index_size
             config.config = WorkerServiceConfig(
                 identity=name,
                 worker_count=2,
@@ -41,7 +49,6 @@ class IndexQueue:
             )
             config.service = self.producer(name, config.config)
             self.store_stage_action(name, config)
-            self._counter_lock = threading.Lock()
 
     def producer(self, action_key: str, config):
         async def action():
@@ -71,9 +78,17 @@ class IndexQueue:
 
     def enqueue(self, action_key: str, key=None, value=None):
         with self._counter_lock:
-            self._in_flight_tasks += 1
 
-            self._pbar.total = max(self._pbar.total, self._in_flight_tasks + self._pbar.n)
+            if action_key not in self._in_flight_tasks:
+                self._in_flight_tasks[action_key] = 0
+                self._max_sizes[action_key] = 100
+
+            while self._in_flight_tasks[action_key] >= self._max_sizes[action_key]:
+                self._counter_lock.wait()
+
+            self._in_flight_tasks[action_key] += 1
+            total_current_tasks = sum(self._in_flight_tasks.values())
+            self._pbar.total = max(self._pbar.total, total_current_tasks + self._pbar.n)
             self._pbar.refresh()
 
             if self._in_flight_tasks == 1:
@@ -84,14 +99,14 @@ class IndexQueue:
 
         self.index.store_in_index(action_key, key, value)
 
-    def task_complete(self):
+    def task_complete(self, action_key: str):
         with self._counter_lock:
-            if self._in_flight_tasks > 0:
-                self._in_flight_tasks -= 1
-
+            if self._in_flight_tasks.get(action_key, 0) > 0:
+                self._in_flight_tasks[action_key] -= 1
                 self._pbar.update(1)
+                self._counter_lock.notify_all()
 
-                if self._in_flight_tasks == 0:
+                if sum(self._in_flight_tasks.values()) == 0:
                     self._loop.call_soon_threadsafe(self._all_tasks_done.set)
 
     async def wait_for_completions(self):
@@ -112,12 +127,13 @@ def new_index_queue(worker_count: int, *actions):
         def a(target_action):
             def b(queue_data: QueueData):
                 index = queue_data.attribute("index_queue")
+                act_key = queue_data.attribute("action_key") or target_action.__name__
                 try:
                     target_action(index, queue_data)
                 except Exception as e:
                     traceback.print_exception(e)
                 finally:
-                    index.task_complete()
+                    index.task_complete(act_key)
             return b
         _actions[x.__name__] = ActionConfig(queue=new_controller(action=a(x), worker_count=worker_count))
 
